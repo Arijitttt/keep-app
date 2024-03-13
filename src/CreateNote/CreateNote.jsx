40ae4ad6092d65c6ee671714d1a057e44b67c3b5
Arijit Bhattacharya <113541100+Arijitttt@users.noreply.github.com>
2024-03-13 20:17:56 +0530

    root ui added

--- src/CreateNote/CreateNote.jsx
+++ src/CreateNote/CreateNote.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { IoAddOutline } from "react-icons/io5";
+import './createnote.css'
 export default function CreateNote() {
   return (
-    <div>
-      note
-    </div>
+    <>
+      <div className="main_note">
+        <form action="">
+            <input type="text" autoComplete='on' name="" id="" placeholder='Title'/>
+            <textarea name="" id="" cols="30" rows="10" placeholder='Write a note...'></textarea>
+            <button><IoAddOutline/></button>
+        </form>
+      </div>
+    </>
   )
 }
